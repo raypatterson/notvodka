7ffbb177a9ba5f9df23d7a5639eec46ff9e64fc0
Ray Patterson <ray.patterson@SFMB55FD57L.local>
2014-10-31 09:53:00 -0700

    Added some styles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var pkg = require('./package');
 var del = require('del');
+var path = require('path');
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var webpack = require('gulp-webpack');
 var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
+var autoprefixer = require('autoprefixer-core');
 var reload = browserSync.reload;
 
 gulp.task('default', function() {
@@ -22,7 +24,7 @@ gulp.task('reload', function() {
 gulp.task('nodemon', function() {
   return nodemon({
       script: 'bin/www',
-      ext: 'js jsx html ejs',
+      ext: 'js jsx html ejs scss',
       ignore: ['public/**/*.*', 'node_modules/']
     })
     .on('start', 'build');
@@ -35,13 +37,19 @@ gulp.task('del', function() {
 gulp.task('build', ['webpack', 'reload']);
 
 gulp.task('webpack', function() {
-  return gulp.src('app/*.jsx')
+  return gulp.src(['app/*.jsx', 'app/sass/*'])
     .pipe(webpack({
       module: {
         loaders: [{
           test: /\.jsx$/,
           loader: 'jsx'
+        }, {
+          test: /\.scss$/,
+          loader: 'style!css!sass?outputStyle=expanded&includePaths[]=' + (path.resolve(__dirname, './public'))
         }]
+      },
+      postcss: {
+        defaults: [autoprefixer],
       },
       output: {
         filename: 'js/bundle.js',
